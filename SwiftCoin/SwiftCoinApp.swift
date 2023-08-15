db031d72240781518f7b0de66b1280d77b886d83
Yigit Esmen <yigit.esmen@gmail.com>
2023-08-15 22:46:49 +0300

    Initial commit

---
+++ SwiftCoin/SwiftCoinApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftCoinApp.swift
+//  SwiftCoin
+//
+//  Created by Yigit Esmen on 8/15/23.
+//
+
+import SwiftUI
+
+@main
+struct SwiftCoinApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
